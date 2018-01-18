93b408e6e905ad1f74ec7c7dbb4075d269042c18
Wilmer Nilsson <wilmernilsson@gmail.com>
2018-01-18 23:11:16 +0100

    yeah buddy

--- src/Main.hs
+++ src/Main.hs
@@ -6,9 +6,14 @@ main = do
 
 
 
+fpi :: Int -> (Double -> Double) -> Double -> [Double]
+fpi  0  _ x = [x]
+fpi for f x = x : fpi (for-1) f y
+  where y = f x
 
-fpi :: (Double -> Double) -> Double -> Double -> Double
-fpi f x margin
+--can get stuck if no fixed points
+fpiMargins :: (Double -> Double) -> Double -> Double -> Double
+fpiMargins f x margin
   | ((<margin).abs.subtract x) y = x
-  | otherwise = fpi f y margin
+  | otherwise = fpiMargins f y margin
   where y = f x
